9581286c24cf55e2336cc86cdac671aa278d5d8e
fangfengxin <1208689216@qq.com>
2020-02-29 16:01:59 +0800

    LeetCode Hot-100 Day05

---
+++ Hot100/src/problem_021_MergeTwoSortedLists/MergeTwoSortedListsTest.java
@@ -0,0 +1,30 @@
+package problem_021_MergeTwoSortedLists;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MergeTwoSortedListsTest {
+    private MergeTwoSortedLists solution;
+    private ListNode l1;
+    private ListNode l2;
+    private ListNode expected;
+
+    @Before
+    public void init() {
+        solution = new MergeTwoSortedLists();
+        l1 = ListNode.addAll(1, 2, 4);
+        l2 = ListNode.addAll(1, 3, 4);
+        expected = ListNode.addAll(1, 1, 2, 3, 4, 4);
+    }
+
+    @Test
+    public void testMergeTwoLists() {
+        ListNode result = solution.mergeTwoLists(l1, l2);
+        if (result == null || expected == null) {
+            Assert.assertEquals(expected, result);
+        } else {
+            Assert.assertEquals(expected.toString(), result.toString());
+        }
+    }
+}
