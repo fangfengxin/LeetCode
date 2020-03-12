fb1854625e8a1b7f7d5ec058434186613baafeb5
fangfengxin <1208689216@qq.com>
2020-03-12 13:57:22 +0800

    LeetCode Hot-100 Day13

--- Hot100/src/problem_0064_MinimumPathSum/MinimumPathSum.java
+++ Hot100/src/problem_0064_MinimumPathSum/MinimumPathSum.java
@@ -87,14 +87,14 @@ public class MinimumPathSum {
     public int minPathSum4(int[][] grid) {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                if (i == 0 && j == 0) {
-                    continue;
-                } else if (i == 0) {
-                    grid[i][j] += grid[i][j - 1];
-                } else if (j == 0) {
-                    grid[i][j] += grid[i - 1][j];
-                } else {
-                    grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+                if (i != 0 || j != 0) {
+                    if (i == 0) {
+                        grid[i][j] += grid[i][j - 1];
+                    } else if (j == 0) {
+                        grid[i][j] += grid[i - 1][j];
+                    } else {
+                        grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+                    }
                 }
             }
         }
