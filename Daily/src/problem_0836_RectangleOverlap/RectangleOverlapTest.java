1dd77d74c340544924e7e6336d4f7b0cae4be380
fangfengxin <1208689216@qq.com>
2020-03-18 16:15:06 +0800

    LeetCode Daily Day13

---
+++ Daily/src/problem_0836_RectangleOverlap/RectangleOverlapTest.java
@@ -0,0 +1,32 @@
+package problem_0836_RectangleOverlap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RectangleOverlapTest {
+    RectangleOverlap solution;
+    int[] rec1, rec2;
+    boolean expected;
+
+    @Before
+    public void setUp() {
+        solution = new RectangleOverlap();
+        rec1 = new int[]{0, 0, 2, 2};
+        rec2 = new int[]{1, 1, 3, 3};
+        expected = true;
+    }
+
+    @Test
+    public void testIsRectangleOverlap1() {
+        boolean result = solution.isRectangleOverlap1(rec1, rec2);
+        assertEquals(expected, result);
+    }
+    @Test
+    public void testIsRectangleOverlap2() {
+        boolean result = solution.isRectangleOverlap2(rec1, rec2);
+        assertEquals(expected, result);
+    }
+
+}
