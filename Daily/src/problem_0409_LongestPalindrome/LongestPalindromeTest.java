218b31967b47e905bf7c8f84fa1cfa45002a3020
fangfengxin <1208689216@qq.com>
2020-03-19 13:48:00 +0800

    LeetCode Daily Day14

---
+++ Daily/src/problem_0409_LongestPalindrome/LongestPalindromeTest.java
@@ -0,0 +1,31 @@
+package problem_0409_LongestPalindrome;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LongestPalindromeTest {
+    LongestPalindrome solution;
+    String s;
+    int expected;
+
+    @Before
+    public void setUp() {
+        solution = new LongestPalindrome();
+        s = "abccccdd";
+        expected = 7;
+    }
+
+    @Test
+    public void testLongestPalindrome1() {
+        int result = solution.longestPalindrome1(s);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testLongestPalindrome2() {
+        int result = solution.longestPalindrome2(s);
+        assertEquals(expected, result);
+    }
+}
