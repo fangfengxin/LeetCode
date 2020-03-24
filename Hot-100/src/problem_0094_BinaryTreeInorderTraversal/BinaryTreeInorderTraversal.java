f47710b44d76cd33f53b15d2ee0801220f9db728
fangfengxin <1208689216@qq.com>
2020-03-24 15:17:02 +0800

    LeetCode Hot-100 Day18

---
+++ Hot-100/src/problem_0094_BinaryTreeInorderTraversal/BinaryTreeInorderTraversal.java
@@ -0,0 +1,58 @@
+package problem_0094_BinaryTreeInorderTraversal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/*
+ * 94. 二叉树的中序遍历
+ * 给定一个二叉树，返回它的中序 遍历。
+ *
+ * 示例:
+ * 输入: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ * 输出: [1,3,2]
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ */
+public class BinaryTreeInorderTraversal {
+    /*
+     * 方法1：递归
+     */
+    public List<Integer> inorderTraversal1(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorderTraversal(root, res);
+        return res;
+    }
+
+    private void inorderTraversal(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left, res);
+        res.add(root.val);
+        inorderTraversal(root.right, res);
+    }
+
+    /*
+     * 方法2：栈
+     */
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            res.add(node.val);
+            node = node.right;
+        }
+        return res;
+    }
+}
---
+++ Hot-100/src/problem_0094_BinaryTreeInorderTraversal/BinaryTreeInorderTraversalTest.java
@@ -0,0 +1,33 @@
+package problem_0094_BinaryTreeInorderTraversal;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class BinaryTreeInorderTraversalTest {
+    BinaryTreeInorderTraversal solution;
+    TreeNode root;
+    List<Integer> expected;
+
+    @Before
+    public void setUp() {
+        solution = new BinaryTreeInorderTraversal();
+        root = TreeNode.of(1, null, 2, 3);
+        expected = List.of(1, 3, 2);
+    }
+
+    @Test
+    public void testInorderTraversal1() {
+        List<Integer> result = solution.inorderTraversal1(root);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testInorderTraversal2() {
+        List<Integer> result = solution.inorderTraversal2(root);
+        assertEquals(expected, result);
+    }
+}
