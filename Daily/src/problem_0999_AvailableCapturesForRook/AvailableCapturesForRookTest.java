3bc58ba4c8ef3301bcbe6f1858baec834cc17149
fangfengxin <1208689216@qq.com>
2020-03-26 12:01:38 +0800

    LeetCode Daily Day20

---
+++ Daily/src/problem_0999_AvailableCapturesForRook/AvailableCapturesForRookTest.java
@@ -0,0 +1,34 @@
+package problem_0999_AvailableCapturesForRook;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AvailableCapturesForRookTest {
+    AvailableCapturesForRook solution;
+    char[][] board;
+    int expected;
+
+    @Before
+    public void setUp() {
+        solution = new AvailableCapturesForRook();
+        board = new char[][]{
+                {'.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', 'p', '.', '.', '.', '.'},
+                {'.', '.', '.', 'p', '.', '.', '.', '.'},
+                {'p', 'p', '.', 'R', '.', 'p', 'B', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', 'B', '.', '.', '.', '.'},
+                {'.', '.', '.', 'p', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.'}
+        };
+        expected = 3;
+    }
+
+    @Test
+    public void numRookCaptures() {
+        int result = solution.numRookCaptures(board);
+        assertEquals(expected, result);
+    }
+}
